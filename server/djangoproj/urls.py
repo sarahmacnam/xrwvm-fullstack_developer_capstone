94ab3246df7f4f2145495d7de7a71d9ddf5db284
sarahmacnam <scdirect2015@gmail.com>
2025-07-07 02:37:18 +0200

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -29,3 +29,4 @@ urlpatterns = [
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
     path(route='login', view=views.login_user, name='login'),
+    path(route='logout', view=views.logout_request, name='logout'),
